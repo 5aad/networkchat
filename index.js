cc23c2b4940f872e5f39d7402dc78b6cb53dff2f
AtiaK <atiakalwar@gmail.com>
2021-04-16 00:55:46 +0500

    done but not tested

--- index.js
+++ index.js
@@ -6,4 +6,40 @@ import {AppRegistry} from 'react-native';
 import App from './Main';
 import {name as appName} from './app.json';
 
+// import PushNotificationIOS from "@react-native-community/push-notification-ios";
+import PushNotification from "react-native-push-notification";
+import {Platform} from 'react-native'
+
+PushNotification.configure({
+    onRegister: function (token) {
+      console.log("TOKEN:", token);
+    },
+      onNotification: function (notification) {
+      console.log("NOTIFICATION:", notification);
+
+    //   notification.finish(PushNotificationIOS.FetchResult.NoData);
+    },
+  
+    // onAction: function (notification) {
+    //   console.log("ACTION:", notification.action);
+    //   console.log("NOTIFICATION:", notification);
+  
+    //   // process the action
+    // },
+  
+    // onRegistrationError: function(err) {
+    //   console.error(err.message, err);
+    // },
+  
+    permissions: {
+      alert: true,
+      badge: true,
+      sound: true,
+    },
+  
+    popInitialNotification: true,
+
+    requestPermissions: Platform.Os==='ios',
+  });
+
 AppRegistry.registerComponent(appName, () => App);
